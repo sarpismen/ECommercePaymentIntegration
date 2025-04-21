beba477a025225b8542aba7e92492b7c0fad8e8a
sarpismen <sarpismen@outlook.com>
2025-04-21 19:42:01 +0300

    resilient http requests

--- ECommercePaymentIntegration.Application/Services/BalanceManagement/BalanceManagementService.cs
+++ ECommercePaymentIntegration.Application/Services/BalanceManagement/BalanceManagementService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Text;
@@ -13,11 +14,22 @@ using ECommercePaymentIntegration.Application.Exceptions;
 using ECommercePaymentIntegration.Application.Interfaces.BalanceManagement;
 using ECommercePaymentIntegration.Application.Json;
 using Microsoft.Extensions.Logging;
+using Polly;
+using Polly.Timeout;
 
 namespace ECommerceApp.Infrastructure.BalanceManagement
 {
    public class BalanceManagementService : IBalanceManagementService
    {
+      private static readonly HashSet<HttpStatusCode> RetryStatusCodes = new HashSet<HttpStatusCode>
+      {
+         HttpStatusCode.RequestTimeout, // 408
+         HttpStatusCode.InternalServerError, // 500
+         HttpStatusCode.BadGateway, // 502
+         HttpStatusCode.ServiceUnavailable, // 503
+         HttpStatusCode.GatewayTimeout, // 504
+      };
+
       private readonly HttpClient _httpClient;
       private readonly ILogger<BalanceManagementService> _logger;
 
@@ -54,41 +66,53 @@ namespace ECommerceApp.Infrastructure.BalanceManagement
 
       private async Task<T> GetAsync<T>(string endpoint)
       {
-         return await HttpOperationAsync<T>(() => _httpClient.GetAsync(endpoint));
+         return await ResilientHttpOperationAsync<T>(() => _httpClient.GetAsync(endpoint));
       }
 
       private async Task<TResponse> PostAsync<TRequest, TResponse>(string endpoint, TRequest request)
       {
-         return await HttpOperationAsync<TResponse>(() => _httpClient.PostAsJsonAsync(endpoint, request, JsonSerializerSettings.BalanceManagementServiceJsonSerializerOptions));
+         return await ResilientHttpOperationAsync<TResponse>(() => _httpClient.PostAsJsonAsync(endpoint, request, JsonSerializerSettings.BalanceManagementServiceJsonSerializerOptions));
+      }
+
+      private async Task<T> ResilientHttpOperationAsync<T>(Func<Task<HttpResponseMessage>> action)
+      {
+         var timeoutPolicy = Policy.TimeoutAsync(TimeSpan.FromSeconds(30), TimeoutStrategy.Optimistic);
+
+         var retryPolicy = Policy
+             .Handle<HttpRequestException>(ex => ex.StatusCode.HasValue && RetryStatusCodes.Contains(ex.StatusCode.Value))
+             .Or<TimeoutRejectedException>()
+             .WaitAndRetryAsync(
+                 retryCount: 5,
+                 sleepDurationProvider: attempt => attempt > 2 ? TimeSpan.FromSeconds(Math.Pow(2, attempt - 2)) : TimeSpan.Zero,
+                 onRetry: (exception, timespan, retryAttempt, context) =>
+                 {
+                    _logger.LogInformation($"Retry #{retryAttempt} due to: {exception.Message}");
+                 });
+         var combinedPolicy = Policy.WrapAsync(retryPolicy, timeoutPolicy);
+         try
+         {
+            var result = await combinedPolicy.ExecuteAsync(async () => await HttpOperationAsync<T>(action));
+            return result;
+         }
+         catch (Exception ex)
+         {
+            throw new BalanceManagementServiceException("An error occured while sending request", ex);
+         }
       }
 
       private async Task<T> HttpOperationAsync<T>(Func<Task<HttpResponseMessage>> action)
       {
+         try
          {
-            try
-            {
-               var response = await action();
-               //Throws if error
-               response.EnsureSuccessStatusCode();
-               var result = await response.Content.ReadFromJsonAsync<Response<T>>(JsonSerializerSettings.BalanceManagementServiceJsonSerializerOptions);
-               return result.Success ? result.Data : throw new BalanceManagementServiceException();
-            }
-            catch (HttpRequestException ex)
-            {
-               _logger.LogError($"Error calling Balance Management API: {ex.Message}");
-               switch (ex.StatusCode)
-               {
-                  case System.Net.HttpStatusCode.NotFound:
-                     return default;
-                  default:
-                     throw new BalanceManagementServiceException("Service returned error", ex);
-               }
-            }
-            catch (Exception ex)
-            {
-               _logger.LogError(ex, "Balance Management Service encountered an unexpected error");
-               throw;
-            }
+            var response = await action();
+            //Throws if error
+            response.EnsureSuccessStatusCode();
+            var result = await response.Content.ReadFromJsonAsync<Response<T>>(JsonSerializerSettings.BalanceManagementServiceJsonSerializerOptions);
+            return result.Success ? result.Data : throw new BalanceManagementServiceException();
+         }
+         catch (HttpRequestException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
+         {
+            return default;
          }
       }
    }
