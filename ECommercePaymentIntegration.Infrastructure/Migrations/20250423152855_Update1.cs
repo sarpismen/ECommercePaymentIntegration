a48d1e63c7a6b653da2c0eec4cbf0488b1129f57
sarpismen <sarpismen@outlook.com>
2025-04-23 18:36:03 +0300

    Migrations

---
+++ ECommercePaymentIntegration.Infrastructure/Migrations/20250423152855_Update1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECommercePaymentIntegration.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ItemPrice",
+                table: "OrderItems",
+                newName: "UnitPrice");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UnitPrice",
+                table: "OrderItems",
+                newName: "ItemPrice");
+        }
+    }
+}
